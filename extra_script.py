ffa04c014c077f1eb4a39e83220b30c16ca58116
Oleksii Kutuzov <oleksii.kutuzov@icloud.com>
2022-09-05 14:27:44 +0200

    Update platformio config

--- extra_script.py
+++ extra_script.py
@@ -4,7 +4,6 @@ from shutil import copyfile
 def move_bin(*args, **kwargs):
     print("Copying bin output to project directory...")
     target = str(kwargs['target'][0])
-    print(target)
     if target == ".pio/build/esp32dev_rgb/firmware.bin":
         copyfile(target, 'esp32_solvinden_rgb.bin')
     elif target == ".pio/build/esp32dev_rgbw/firmware.bin":
